6dc7a989982eef4263577a55756a870b7ff3430d
lassebenni <lasse_benninga@hotmail.com>
2015-12-03 11:17:39 +0100

    insert script sql

--- week-2/javabank_create_script.sql
+++ week-2/javabank_create_script.sql
@@ -53,3 +53,5 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+INSERT INTO `accounts` (`balance_amount`,`account_number`,`name`,`address`,`city`,`limit_amount`) VALUES (967,"CY60979006261151065725792620","Quinlan Bailey","964-2275 Arcu St.","Bludenz",-1000),(675,"IS441552425328519418046062","Lamar Griffith","523-9952 Eu St.","Dilbeek",-1000),(333,"TR204462629183112678416120","Colorado Hoffman","443 Commodo Street","Monghidoro",-1000),(849,"PK2911552680717413414670","Armand Mendez","Ap #666-525 Mauris Road","Whitehorse",-1000),(899,"TN2854634633871565807838","Erasmus Stanton","Ap #319-4219 Proin St.","Wakefield",-1000),(742,"BA738954050563062856","Kenyon Sheppard","612-2873 A St.","Pemberton",-1000),(85,"MK28461563713343737","Steel Estes","P.O. Box 875, 1467 Sed St.","Sivry",-1000),(463,"IL243099131696595130581","Cameron Camacho","1550 Lacus. Road","Halifax",-1000),(815,"CZ7461913047485683490923","Nathaniel Hall","1232 Ac St.","Freising",-1000),(547,"IS379855227378154218115298","Garth Benson","4553 Arcu. Ave","Stirling",-1000);
